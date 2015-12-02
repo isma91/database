0452cb17db9b105a6880ca1be86039f8ab320875
isma91 <ismaydogmus@gmail.com>
2015-12-02 09:45:24 +0100

    maj aide pour lacer le script comme un shell @devraph0

--- database.php
+++ database.php
@@ -65,39 +65,38 @@ function function_debug_mysql ($host, $username, $password, $database) {
 }
 function not_argv_enough ($type) {
     echo "\033[1;33m\033[40mPas assez d'argument !!\033[0m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32m" . $type . "\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32m" . $type . "\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
 }
 function too_much_argv_enough ($type) {
     echo "\033[1;33m\033[40mTrop d'argument !!\033[0m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32m" . $type . "\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32m" . $type . "\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
 }
 if (count($argv) === 1) {
     echo "\033[1;33m\033[40mPas d'argument !!\033[0m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32mhelp\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
 } elseif ($argv[1] === "help") {
     echo "\033[1;37m\033[40mAide pour utiliser ce script PHP :\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32mliste\033[40m\033[1;37m pour avoir la liste des types de base de donnée compatibles !!\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32mliste\033[40m\033[1;37m pour avoir la liste des types de base de donnée compatibles !!\n";
     echo "\033[40m\n";
-    echo "\033[40m\033[1;37mtapez\033[1;31m php " . $GLOBALS["filename"] . " \033[1;32m[type]\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec le type de base de donnée choisit !!\033[0m\n";
+    echo "\033[40m\033[1;37mtapez\033[1;31m ./" . $GLOBALS["filename"] . " \033[1;32m[type]\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec le type de base de donnée choisit !!\033[0m\n";
     echo "\033[40m\033[1;37mExemple :\n";
-    echo "\033[1;31mphp " . $GLOBALS["filename"] . " \033[1;32mmysql\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
+    echo "\033[1;31m./" . $GLOBALS["filename"] . " \033[1;32mmysql\033[40m\033[1;37m pour avoir l'aide pour utiliser le script avec mysql\033[0m\n";
 } elseif ($argv[1] === "liste") {
     echo "\033[1;37m\033[40mVoici la liste des types de base de donnée supporter :\n";
     echo "\033[1;32mmysql\033[0m\n";
 } elseif (strtolower($argv[1]) === "mysql") {
     if (count($argv) === 2) {
         echo "\033[1;37m\033[40mAide pour utiliser le script avec mysql :\n";
-        echo "\033[1;31mphp " . $GLOBALS["filename"] . " \033[1;32mmysql [host] [username] [password] [database]\n";
+        echo "\033[40m\033[1;37mOption :\n";
+        echo "\033[1;31m./" . $GLOBALS["filename"] . " \033[1;32mmysql [host] [username] [password] [database]\n";
         echo "\033[1;31mtapez \033[1;32mdebug\033[1;31m avant \033[1;32mmysql \033[1;37m pour avoir les erreurs originelle\n";
-        echo "\033[1;32m[type]\033[1;37m     => le type de base de donnée (ici mysql)\n";
-        echo "\033[1;32m[host]\033[1;37m     => le serveur (localhost si vous êtes en dev)\n";
-        echo "\033[1;32m[username]\033[1;37m => le nom d'utilisateur pour ce connecter à votre mysql\n";
-        echo "\033[1;32m[password]\033[1;37m => le mot de passe pour ce connecter à votre mysql \033[1;31mecrire [] si vous avez un mot de passe vide !!\n";
+        echo "\033[1;32m[type]\033[1;37m     => le type de base de donnée (ici mysql) \033[1;31mArgument obligatoire\n";
+        echo "\033[1;32m[host]\033[1;37m     => le serveur (localhost si vous êtes en dev) \033[1;31mArgument obligatoire\n";
+        echo "\033[1;32m[username]\033[1;37m => le nom d'utilisateur pour ce connecter à votre mysql \033[1;31mArgument obligatoire\n";
+        echo "\033[1;32m[password]\033[1;37m => le mot de passe pour ce connecter à votre mysql \033[1;31mecrire [] si vous avez un mot de passe vide !! \033[1;31mArgument obligatoire\n";
         echo "\033[1;32m[database]\033[1;37m => le nom de la base de donnée que vous voulez crée\033[0m\n";
-        echo "\033[40m\033[1;37mExemple :\n";
-        echo "\033[1;31mphp " . $GLOBALS["filename"] . " \033[1;32mmysql localhost root pass bddtest\033[40m\033[1;37m\033[0m\n";
     } elseif (count($argv) < 6) {
         not_argv_enough("mysql");
     } elseif (count($argv) === 6) {
@@ -125,6 +124,6 @@ if (count($argv) === 1) {
         }
     }
 } else {
-    echo "\033[1;33m\033[40mErreur !! \033[40m\033[1;37mTapez\033[1;31m php " . $GLOBALS["filename"] . " help\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
+    echo "\033[1;33m\033[40mErreur !! \033[40m\033[1;37mTapez\033[1;31m ./" . $GLOBALS["filename"] . " help\033[40m\033[1;37m pour avoir de l'aide !!\033[0m\n";
 }
 ?>
